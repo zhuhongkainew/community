689c05a8e29764e900180a087d1d461a20e7e1dc
zhuhongkainew <zyy1990913@163.com>
2021-09-20 21:47:25 +0800

    增加登录

--- src/main/java/life/zhk/community/controller/LoginController.java
+++ src/main/java/life/zhk/community/controller/LoginController.java
@@ -1,42 +1,44 @@
 package life.zhk.community.controller;
 
-import life.zhk.community.mapper.UserMapper;
+import cn.hutool.crypto.SecureUtil;
 import life.zhk.community.model.User;
+import life.zhk.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
-@Controller
+@RestController
 public class LoginController {
+
     @Autowired
-    private UserMapper userMapper;
-
-//    @GetMapping("/goLogin")
-//    public String login() {
-//        return "login";
-//    }
-
-    @GetMapping("/login")
-    public String sign(@RequestParam(name = "email") String id,
-                       @RequestParam(name = "password") String name,
-                       HttpServletRequest request,
-                       HttpServletResponse response
+    private UserService userService;
+    @PostMapping("/login")
+    public Map<String,Object> sign(User user,
+                    HttpServletResponse response
 
     ) {
+        Map<String,Object> objectMap =userService.loginUser(user,response);
         //User user = userMapper.findById(Integer.parseInt(name));
-        User user = userMapper.selectByPrimaryKey(Integer.parseInt(name));
+        //User user1 = userMapper.selectByPrimaryKey(Integer.parseInt(user.getName()));
 
-        if (user != null) {
-            response.addCookie(new Cookie("token", user.getToken()));
-            return "redirect:/";
-        } else {
-            return "redirect:/login";
-        }
+//        if (user != null) {
+//            response.addCookie(new Cookie("token", user1.getToken()));
+//
+//        } else {
+//
+//        }
 
+    return objectMap;
+    }
+
+    public static void main(String[] args) {
+        String a =SecureUtil.md5("12345678" + "bkia8q");
+        System.out.println(a);
     }
 }
--- src/main/java/life/zhk/community/service/UserService.java
+++ src/main/java/life/zhk/community/service/UserService.java
@@ -4,10 +4,14 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.SecureUtil;
 import life.zhk.community.mapper.UserMapper;
 import life.zhk.community.model.User;
+import life.zhk.community.model.UserExample;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -17,8 +21,17 @@ public class UserService {
     private UserMapper userMapper;
 
     public Map<String, Object> register(User user) {
-        String salt= RandomUtil.randomString(6);
-        String md5Pwd= SecureUtil.md5(user.getPassword()+salt);
+        Map<String, Object> objectMap = new HashMap<>();
+        UserExample example = new UserExample();
+        example.createCriteria().andAccountIdEqualTo(user.getAccountId());
+        List<User> userSize = userMapper.selectByExample(example);
+        if (userSize.size() > 0) {
+            objectMap.put("code", 400);
+            objectMap.put("message", "注册失败,帐号已被占用，请换一个！");
+            return objectMap;
+        }
+        String salt = RandomUtil.randomString(6);
+        String md5Pwd = SecureUtil.md5(user.getPassword() + salt);
         user.setPassword(md5Pwd);
         user.setSalt(salt);
         user.setGmtCreate(System.currentTimeMillis());
@@ -27,16 +40,51 @@ public class UserService {
         //String token = IdUtil.getSnowflake(1,1).nextIdStr();
         String token = UUID.randomUUID().toString();
         user.setToken(token);
-        Map<String,Object> objectMap =new HashMap<>();
-        int result=userMapper.insert(user);
-        if(result>0){
-            objectMap.put("code",200);
-            objectMap.put("message","注册成功");
-        }else{
-            objectMap.put("code",400);
-            objectMap.put("message","注册失败");
+
+        int result = userMapper.insert(user);
+        if (result > 0) {
+            objectMap.put("code", 200);
+            objectMap.put("message", "注册成功，请登录");
+        } else {
+            objectMap.put("code", 400);
+            objectMap.put("message", "注册失败");
         }
         return objectMap;
     }
 
+    public Map<String, Object> loginUser(User user, HttpServletResponse httpServletResponse) {
+        Map<String, Object> objectMap = new HashMap<>();
+        UserExample example = new UserExample();
+        example.createCriteria().andAccountIdEqualTo(user.getAccountId());
+        List<User> userList = userMapper.selectByExample(example);
+        if (userList.size() == 0) {
+            objectMap.put("code", "400");
+            objectMap.put("message", "用户名不存在");
+            return objectMap;
+        }
+        if (userList.size() > 1) {
+            objectMap.put("code", "400");
+            objectMap.put("message", "帐号异常，请联系管理员");
+            return objectMap;
+        }
+        String salt =userList.get(0).getSalt();
+        String pwd =user.getPassword();
+        String password = SecureUtil.md5(user.getPassword() + salt);
+        UserExample userExample =new UserExample();
+        userExample.createCriteria().andAccountIdEqualTo(user.getAccountId()).andPasswordEqualTo(password);
+        List<User> userList1  = userMapper.selectByExample(userExample);
+        if (userList1.size() == 0) {
+            objectMap.put("code", "400");
+            objectMap.put("message", "密码错误，请重试");
+            return objectMap;
+        }
+        if (userList1.size() == 1) {
+            objectMap.put("code", "200");
+            objectMap.put("message", "登录成功");
+            httpServletResponse.addCookie(new Cookie("token", userList1.get(0).getToken()));
+
+            return objectMap;
+        }
+        return objectMap;
+    }
 }
